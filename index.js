4c63e178c5ce56163e0e0b56db6e1e6b165850aa
“Kurt <chaconsaavedrakurt@gmailcomgit config --global user.name Kurt Chacongit config --global user.email chaconsaavedrakurt@gmailcom>
2024-10-16 15:01:46 +0200

    solved lab

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-// Iteration 1: Names and Input
-
-
+const hacker1 = "Pedro";
+const hacker2 = "Kurt";
 // Iteration 2: Conditionals
-
-
-// Iteration 3: Loops
+let count1 = 0;
+for (let i = 0; i < hacker1.length; i++) {
+    count1++;
+}
+let count2 = 0;
+for (let i = 0; i < hacker2.length; i++) {
+    count2++;
+}
+if (count1 > count2) {
+    console.log(`The driver has the longest name, it has ${count1} characters.`);
+} else if (count2 > count1) {
+    console.log(`It seems that the navigator has the longest name, it has ${count2} characters.`);
+} else if (count2 === count1) {
+    console.log(`Wow, you both have equally long names, ${count1} characters!`);
+}
